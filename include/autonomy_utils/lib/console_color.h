1335c4edac2564f62992f4c7bd70bff63c1c4990
AJ <ajanani@terabase.energy>
2025-03-13 09:55:26 -0700

    feat: more files

---
+++ include/autonomy_utils/lib/console_color.h
@@ -0,0 +1,21 @@
+
+#ifndef AUTONOMY_UTILS_LIB_CONSOLE_COLOR_H_
+#define AUTONOMY_UTILS_LIB_CONSOLE_COLOR_H_
+
+#include <stdio.h>
+
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define BRED	"\x1b[31;1m"
+#define BGREEN   "\x1b[32;1m"
+#define BYELLOW  "\x1b[33;1m"
+#define BBLUE    "\x1b[34;1m"
+#define BMAGENTA "\x1b[35;1m"
+#define BCYAN    "\x1b[36;1m"
+#define COLOR_RESET   "\x1b[0m"
+
+#endif /*AUTONOMY_UTILS_LIB_CONSOLE_COLOR_H_*/
---
+++ include/autonomy_utils/lib/ros2_core.h
@@ -0,0 +1,9 @@
+#ifndef AUTONOMY_UTILS_LIB_ROS2_CORE_H_
+#define AUTONOMY_UTILS_LIB_ROS2_CORE_H_
+
+#include <autonomy_utils/lib/ros2_params.h>
+#include <autonomy_utils/lib/ros2_utils.h>
+#include <autonomy_utils/lib/ros2_nodes.h>
+#include <autonomy_utils/lib/console_color.h>
+
+#endif /*AUTONOMY_UTILS_LIB_ROS2_CORE_H_*/
