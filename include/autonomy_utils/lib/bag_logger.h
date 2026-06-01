d7663b771635c512b518a442a92889d9b4c5b76e
Alireza Janani <ajanani85@gmail.com>
2026-05-31 19:00:02 -0700

    feat: bag logger will write into /var/log/data and /media/data

--- include/autonomy_utils/lib/bag_logger.h
+++ include/autonomy_utils/lib/bag_logger.h
@@ -19,8 +19,8 @@
 #include <rosbag2_storage/storage_options.hpp>
 
 
-#define DEFAULT_BAG_DIR "/var/log/terabase/"
-#define DEFAULT_MEDIA_DIR "/media/terabase"
+#define DEFAULT_BAG_DIR "/var/log/data/"
+#define DEFAULT_MEDIA_DIR "/media/data"
 
 
 //in ros2 you cannot do ros::Time::now(). You have to send the time over
